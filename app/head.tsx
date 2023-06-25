2a3e84baf144bec6312644220cd285181a9d7825
admin300400 <rezfajrul@gmail.com>
2023-06-25 22:27:25 +0700

    metatag

---
+++ app/head.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const head = () => {
+  return (
+    <>
+    <meta property="og:title" content="Bidan okti" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://www.klinikbidanokti.com/" />
+    <meta
+      property="og:description"
+      content="Fasilitas
+    "
+    />
+    <meta itemProp="name" content="Bidan Okti" />
+    <meta
+      itemProp="description"
+      content="Fasilitas
+    "
+    />
+    <meta itemProp="url" content="https://www.klinikbidanokti.com/" />
+    <meta
+      itemProp="thumbnailUrl"
+      content="/profile.jpg"
+    />
+    <meta
+      itemProp="image"
+      content="/profile.jpg"
+    />
+    <meta
+      itemProp="imageUrl"
+      content="/profile.jpg"
+    />
+    <meta
+      property="og:image"
+      content="/profile.jpg"
+    />
+    </>
+  )
+}
+
+export default head
