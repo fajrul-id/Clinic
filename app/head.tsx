f3bf734dcb4ccc4b99b14ee23be688f453e4ee2c
admin300400 <rezfajrul@gmail.com>
2023-06-26 15:24:47 +0700

    seo

--- app/head.tsx
+++ app/head.tsx
@@ -1,42 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Head = () => {
   return (
     <>
-    <title>Klinik Bidan Okti Susanti</title>
-    <meta property="og:title" content="Bidan okti" />
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://www.klinikbidanokti.com/" />
-    <meta
-      property="og:description"
-      content="Fasilitas
+      <title>Klinik Bidan Okti Susanti</title>
+      <meta property="og:site_name" content="Klinik BOS (Bidan Okti Susanti)" />
+      <meta property="og:title" content="Klinik Bidan Okti Susanti" />
+      <meta property="og:description" content="Cek Lebih Lanjut..." />
+      <meta property="og:image" itemProp="image" content="/profile.jpg" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Bidan okti" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.klinikbidanokti.com/" />
+      <meta itemProp="name" content="Klinik BOS (Bidan Okti Susanti)" />
+      <meta
+        itemProp="description"
+        content="Cek Lebih Lanjut....
     "
-    />
-    <meta itemProp="name" content="Bidan Okti" />
-    <meta
-      itemProp="description"
-      content="Fasilitas
-    "
-    />
-    <meta itemProp="url" content="https://www.klinikbidanokti.com/" />
-    <meta
-      itemProp="thumbnailUrl"
-      content="/profile.jpg"
-    />
-    <meta
-      itemProp="image"
-      content="/profile.jpg"
-    />
-    <meta
-      itemProp="imageUrl"
-      content="/profile.jpg"
-    />
-    <meta
-      property="og:image"
-      content="/profile.jpg"
-    />
+      />
+      <meta itemProp="url" content="https://www.klinikbidanokti.com/" />
+      <meta itemProp="thumbnailUrl" content="/profile.jpg" />
+      <meta itemProp="image" content="/profile.jpg" />
+      <meta itemProp="imageUrl" content="/profile.jpg" />
+      <meta property="og:image" content="/profile.jpg" />
     </>
-  )
-}
+  );
+};
 
-export default Head
+export default Head;
